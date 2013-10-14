1eab4dbc0343807fce0ee4b52b2c00b4065ba828
Ryan A. March <rmarch03@villanova.edu>
2013-10-14 12:38:48 -0400

    First Commit

---
+++ ShapeShift/ShapeShift/Game1.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ShapeShift
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Player player1;
+        Texture2D playerTexture;
+
+        int screenWidth, screenHeight;
+        const int HUDHEIGHT = 50, ABSZERO = 0;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player1 = new Player(240, 240);
+            playerTexture = Content.Load<Texture2D>("Lain");
+
+            screenHeight = GraphicsDevice.Viewport.Height;
+            screenWidth = GraphicsDevice.Viewport.Width;
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                player1.moveUp();
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                player1.moveDown();
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                player1.moveLeft();
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                player1.moveRight();
+
+            if (player1.getX() <= ABSZERO)
+                player1.setX(ABSZERO);
+            if (player1.getX() >= screenWidth - player1.getWidth())
+                player1.setX(screenWidth - player1.getWidth());
+
+            if (player1.getY() <= HUDHEIGHT)
+                player1.setY(HUDHEIGHT);
+            if (player1.getY() >= screenHeight - player1.getHeight())
+                player1.setY(screenHeight - player1.getHeight());
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(playerTexture, player1.getRect(), Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ShapeShift/ShapeShift/Player.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShapeShift
+{
+    class Player
+    {
+        private int  health;
+        private const int FULL = 3, MID = 2, LOW = 1, EMPTY = 0, SIZE = 60, MOVE = 5;
+        private Rectangle rectangle;
+        
+        public Player(int x, int y)
+        {
+            health = FULL;
+            rectangle = new Rectangle(x, y, SIZE, SIZE);
+        }
+
+        public int getX()
+        {
+            return rectangle.X;
+        }
+
+        public int getY()
+        {
+            return rectangle.Y;
+        }
+
+        public int getWidth()
+        {
+            return SIZE;
+        }
+
+        public int getHeight()
+        {
+            return SIZE;
+        }
+
+        public Rectangle getRect()
+        {
+            return rectangle;
+        }
+
+        public void setX(int x)
+        {
+            rectangle.X = x;
+        }
+
+        public void setY(int y)
+        {
+            rectangle.Y = y;
+        }
+
+        public void moveLeft()
+        {
+            rectangle.X -= MOVE;
+        }
+
+        public void moveRight()
+        {
+            rectangle.X += MOVE;
+        }
+
+        public void moveUp()
+        {
+            rectangle.Y -= MOVE;
+        }
+
+        public void moveDown()
+        {
+            rectangle.Y += MOVE;
+        }
+
+        public void takeDamage()
+        {
+            health--;
+            if (health == EMPTY)
+                die();
+        }
+
+        public void die()
+        {
+        }
+
+    }
+}
